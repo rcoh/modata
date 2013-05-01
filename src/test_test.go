4c8faeda12d5c97a9a4bedabcb8560f656328b89
Merritt Boyd <mboyd@mit.edu>
2013-05-01 01:46:16 -0400

    k-buckets: update, find closest, find shortlist

--- src/test_test.go
+++ src/test_test.go
@@ -1,6 +1,5 @@
 package modata
 
-import "bytes"
 import "testing"
 import "fmt"
 import "time"
@@ -27,18 +26,65 @@ func TestReplication(t *testing.T) {
     fmt.Printf("... Pass \n")
 }
 
-func TestUtilXOR(t *testing.T) {
-    fmt.Println("Testing XOR")
+func TestDistance(t *testing.T) {
+    a := NodeID{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}
+    b := NodeID{128, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}
 
-    x := XOR([]byte("123"), []byte("123"))
-    xg := []byte{0,0,0}
-    y := XOR([]byte("012"), []byte("112"))
-    yg := []byte{1,0,0}
-    if (bytes.Compare(x, xg) != 0) {
-        fmt.Println("Bad Xor on equal items")
+    c := NodeID{128, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+    if Distance(&a, &b) != c {
+        t.Errorf("XOR: %v\n", Distance(&a, &b))
     }
-    if (bytes.Compare(y, yg) != 0) {
-        fmt.Println("Bad Xor on unequal items")
-    }
+}
+
+func TestBucketing(t *testing.T) {
+    a := NodeID{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    b := NodeID{128, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    rt := RoutingTable{}
+    rt.me = a
+    fmt.Printf("Bucket %d\n", rt.BucketForNode(b))
+}
+
+func ExampleUpdate() {
+    a := NodeID{128, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    b := NodeID{192, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    rt := RoutingTable{}
+    rt.k = 20
+    rt.me = a
+
+    c1 := Contact{b, "", 0}
+    rt.Update(c1)
+
+    // Make sure 
+    fmt.Printf("Update 1: Size of bucket 1: %d\n", rt.buckets[1].Len())
+
+    c2 := Contact{b, "", 0}
+    rt.Update(c2)
+
+    fmt.Printf("Update 2: Size of bucket 1: %d\n", rt.buckets[1].Len())
+
+    // Output: Update 1: Size of bucket 1: 1
+    // Update 2: Size of bucket 1: 1
+}
+
+func TestShortlist(t *testing.T) {
+    a := NodeID{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+    k := NodeID{64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+    b := NodeID{64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}
+    
+    rt := RoutingTable{}
+    rt.k = 20
+    rt.me = a
+
+    c := Contact{b, "", 0}
+    rt.Update(c)
+    c.port = 1
+    rt.Update(c)
+
+    fmt.Printf("%v\n", rt.buckets[1].Front().Value);
+
+    fmt.Printf("%v\n", rt.SelectShortlist(k, 2))
 
 }
