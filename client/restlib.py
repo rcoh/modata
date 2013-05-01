81f9f1b6f1d8790d71b2d17ffc9d03ab3285781d
Joseph Lynch <jolynch@mit.edu>
2013-05-01 11:39:18 -0400

    Fix json() from json

--- client/restlib.py
+++ client/restlib.py
@@ -2,14 +2,14 @@ import requests
 import hashlib
 SERVER = "http://localhost:1234/"
 def store(chunk, digest):
-    return requests.post(SERVER + "store", data={'key': digest, 'data': chunk}).json
+    return requests.post(SERVER + "store", data={'key': digest, 'data': chunk}).json()
 
 def store_auto(chunk):
     return store(chunk, digest_for_chunk(chunk))
 
 
 def findvalue(digest):
-    resp = requests.get(SERVER + "find-value/" + digest).json
+    resp = requests.get(SERVER + "find-value/" + digest).json()
     if resp['status'] == 'OK':
         return resp['data']
     else:
