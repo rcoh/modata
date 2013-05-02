6b8062fc70125077c8ea16644742a1c3d14781bc
Russell Cohen <rcoh@mit.edu>
2013-05-02 01:28:32 -0400

    Adding more tests.

--- client/restlib.py
+++ client/restlib.py
@@ -1,19 +1,19 @@
 import requests
 import hashlib
 SERVER = "http://localhost:1234/"
-def store(chunk, digest, local=True):
+def store(chunk, digest, local=True, server=SERVER):
     if not local:
         dist = "distributed/"
     else:
         dist = ""
-    return requests.post(SERVER + dist + "store", data={'key': digest, 'data': chunk}).json()
+    return requests.post(server + dist + "store", data={'key': digest, 'data': chunk}).json()
 
-def findvalue(digest, local=True):
+def findvalue(digest, local=True, server=SERVER):
     if not local:
         dist = "distributed/"
     else:
         dist = ""
-    resp = requests.get(SERVER + dist + "find-value/" + digest).json()
+    resp = requests.get(server + dist + "find-value/" + digest).json()
     if resp['status'] == 'OK':
         return resp['data']
     else:
