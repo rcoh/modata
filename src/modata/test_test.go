01426c2ab8e61138e36088cf8ff26f432ca26d3c
Merritt Boyd <mboyd@mit.edu>
2013-05-02 00:48:51 -0400

    Return pings

--- src/modata/test_test.go
+++ src/modata/test_test.go
@@ -34,6 +34,8 @@ func TestBlock(t *testing.T) {
     }
     fmt.Println(nodeid)
 
+    status, data, nodeid = JsonGet("http://localhost:1234/ping")
+
     // a := NodeID{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
     b := NodeID{64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}
     c := NodeID{64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2}
